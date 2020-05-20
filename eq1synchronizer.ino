c4ad2ca61b0739e3cd9560331083b138217b8404
Jacek Olearczyk <rurku@rurku.com>
2020-05-21 00:57:01 +0200

    Added autoguiding capability by accepting commands from serial interface

--- eq1synchronizer.ino
+++ eq1synchronizer.ino
@@ -1,15 +1,16 @@
 // upload command
-// arduino_debug.exe --upload eq1synchronizer.ino --board myboards:avr:uno1Mhz 2>&1 | grep -v StatusLogger
+// arduino_debug.exe --upload eq1synchronizer.ino --board myboards:avr:uno1Mhz --port COM4 2>&1 | grep -v StatusLogger
 
 // Circuit design https://easyeda.com/rurku/eq2-motor-clock-extension
-
-#include <LowPower.h>
-
-//#define DEBUG
 
 #define ASSR_READY_MASK (_BV(TCN2UB) | _BV(OCR2AUB) | _BV(OCR2BUB) | _BV(TCR2AUB) | _BV(TCR2BUB))
 #define PIN_FAST A5
 #define PIN_PWM 6
+
+typedef enum OpMode {
+  Tracking,
+  Override
+} OpMode;
 
 
 unsigned long volatile totalCount = 0;
@@ -17,9 +18,7 @@ unsigned long volatile totalCount = 0;
 void setup() {
   // put your setup code here, to run once:
 
-  #ifdef DEBUG
   Serial.begin(9600);           //  setup serial
-  #endif
 
   //pinMode(5, INPUT_PULLUP); // Pin 5 is T1 in atmega328 - timer 1 external clock input
   pinMode(LED_BUILTIN, OUTPUT);
@@ -70,11 +69,6 @@ void setup() {
   totalCount = 0;
   TIMSK1 = (1 << TOIE1); // enable overflow interrupt
 
-  #ifndef DEBUG
-  // Disable interrupt of timer0 - it's used for time functions like micros() and delay() but we're not using them (i think)
-  TIMSK0 &= ~_BV(TOIE0);
-  #endif
-
   sei();
 
 }
@@ -119,92 +113,236 @@ unsigned long getTotalCount()
   return total;
 }
 
+
 int count = 0;
+
+unsigned char duty;
 
 // Motor makes 1 rotation in approx. 137100 pulses
 // worm gear has 100 teeth, so we want 13710000 pulses per earth's rotation
 // Earth makes a full rotation in 86164s
-#define PULSES_PER_SECOND 159.115175711434 //159.115175711434
+float trackingRate = 159.115175711434;
 
-unsigned char duty = 40;
-unsigned long diff;
+unsigned char trackingDuty = 40;
+unsigned char overrideDuty = 0;
 
+OpMode opMode = Tracking;
+bool fast; // last status of fast button
 bool isSync = false;
 unsigned long syncPulses;
 unsigned long syncSeconds;
 
 unsigned long countLastSecond = 0;
 
+void track() {
+  opMode = Tracking;
+  isSync = false;
+  syncPulses = 0;
+  syncSeconds = 0;
+}
+
+unsigned long overrideStartMillis;
+unsigned long overrideMilliseconds;
+void override(unsigned char duty, unsigned long milliseconds)
+{
+  opMode = Override;
+  overrideDuty = duty;
+  overrideStartMillis = millis();
+  overrideMilliseconds = milliseconds;
+}
+
+void writeMetrics(long diff) {
+  char status = '!';
+  if (opMode == Override) {
+    status = 'o'; // override
+  } else if (opMode == Tracking) {
+    if (isSync) {
+      status = 't'; // tracking
+    } else {
+      status = 's'; // seeking
+    }
+  }
+  Serial.print(status);
+  Serial.print(diff);
+  Serial.print(' ');
+  Serial.println(duty);
+}
+
+void optimizeTrackingDuty(long diff) {
+  if (abs(diff - trackingRate) / trackingRate < 0.05) {
+    isSync = true;
+    syncPulses += diff;
+    syncSeconds += 1;
+    if (syncPulses > trackingRate * syncSeconds) {
+      if (diff > trackingRate) {
+        trackingDuty = max(trackingDuty - 1, 0);
+      }
+    } 
+    else {
+      if (diff < trackingRate) {
+        trackingDuty = min(trackingDuty + 1, 255);
+      }
+    }
+  } 
+  else {
+    isSync = false;
+    syncPulses = 0;
+    syncSeconds = 0;
+    if (diff > 0) { // Make adjustments only if the motor is actually running
+      if (diff > trackingRate) {
+        trackingDuty = max(trackingDuty - 1, 0);
+      }
+      else {
+        trackingDuty = min(trackingDuty + 1, 255);
+      }
+    }
+  }
+}
+
+char commandBuffer[20];
+unsigned char commandPos;
+unsigned char readPos;
+
+long readInt() {
+  // skip leading whitespace
+  while (readPos < commandPos && isWhitespace(commandBuffer[readPos])) {
+    readPos++;
+  }
+  bool isNegative = false;
+  long result = 0;
+  if (readPos < commandPos) {
+    if (commandBuffer[readPos] == '-') {
+      isNegative = true;
+      readPos++;
+    }
+    
+    while (readPos < commandPos && isDigit(commandBuffer[readPos])) {
+      result = result * 10 + commandBuffer[readPos] - '0';
+      readPos++;
+    }
+  }
+  if (isNegative) {
+    return -result;
+  } else {
+    return result;
+  }
+}
+
+float readFloat() {
+  // skip leading whitespace
+  while (readPos < commandPos && isWhitespace(commandBuffer[readPos])) {
+    readPos++;
+  }
+  bool isNegative = false;
+  float result = 0;
+  if (readPos < commandPos) {
+    if (commandBuffer[readPos] == '-') {
+      isNegative = true;
+      readPos++;
+    }
+    
+    while (readPos < commandPos && isDigit(commandBuffer[readPos])) {
+      result = result * 10 + commandBuffer[readPos] - '0';
+      readPos++;
+    }
+    if (readPos < commandPos && commandBuffer[readPos] == '.') {
+      readPos++;
+      // now read the fractional part
+      float weight = 0.1;
+      while (readPos < commandPos && isDigit(commandBuffer[readPos])) {
+        result = result + (commandBuffer[readPos] - '0') * weight;
+        weight = weight / 10;
+        readPos++;
+      }
+    }
+  }
+
+  if (isNegative) {
+    return -result;
+  } else {
+    return result;
+  }
+}
+
+void processCommand() {
+  if (Serial.available() > 0) {
+    int c = Serial.read();
+    if (c == '\n' || c == '\r') {
+      // command complete - process it
+      if (commandPos > 0) {
+        if (commandBuffer[0] == 'o') { // override format o [duty(unsigned char)] [milliseconds (unsigned long)]
+          readPos = 1;
+          unsigned char ovrDuty = readInt();
+          unsigned long ovrMilliseconds = readInt();
+          override(ovrDuty, ovrMilliseconds);
+          Serial.println("ack");
+        }
+        else if (commandBuffer[0] == 't' ) { // track format t [trackingRate(float)]}
+          readPos = 1;
+          trackingRate = readFloat();
+          track();
+          Serial.println("ack");
+        }
+      }
+      commandPos = 0;
+      readPos = 0;
+    }
+    else {
+      if (commandPos < sizeof(commandBuffer)) {
+        commandBuffer[commandPos] = (unsigned char)c;
+        commandPos ++;
+      }
+    }
+  }
+}
+
 void loop() {
   unsigned long count;
-  bool fast = digitalRead(PIN_FAST) == LOW;
-  if (countReady && !fast) {
+  if (countReady) {
     cli();
     count = countThisSecond;
     countReady = false;
     sei();
-
     long diff = count - countLastSecond;
-    if (abs(diff - PULSES_PER_SECOND) / PULSES_PER_SECOND < 0.05) 
-    {
-      isSync = true;
-      syncPulses += diff;
-      syncSeconds += 1;
-      if (syncPulses > PULSES_PER_SECOND * syncSeconds) 
-      {
-        if (diff > PULSES_PER_SECOND)
-        {
-          duty = max(duty - 1, 20);
-        }
-      } 
-      else
-      {
-        if (diff < PULSES_PER_SECOND)
-        {
-          duty = min(duty + 1, 255);
-        }
-      }
-    } 
-    else
-    {
-      isSync = false;
-      syncPulses = 0;
-      syncSeconds = 0;
-      if (diff > 0) // Make adjustments only if the motor is actually running
-      {
-        if (diff > PULSES_PER_SECOND)
-        {
-          duty = max(duty - 1, 20);
-        }
-        else
-        {
-          duty = min(duty + 1, 255);
-        }
-      }  
-    }
-
     countLastSecond = count;
 
-    analogWrite(PIN_PWM, duty);
+    writeMetrics(diff);
+
+    if (opMode == Tracking)
+    {
+      optimizeTrackingDuty(diff);
+    }
+
     // the LED will toggle in TIMER2_COMPA_vect. If isSync = true then LED will be on for 1/8s and off for the rest of the cycle. If isSync = false then LED will be off for 1/8s and on for the rest of the cycle.
     digitalWrite(LED_BUILTIN, isSync ? HIGH : LOW);
-    
-    #ifdef DEBUG
-    Serial.print(diff);
-    Serial.print(' ');
-    Serial.println(duty);
-    #endif //DEBUG
   }
 
-  if (fast) 
-  {
-    analogWrite(PIN_PWM, 255);
-    isSync = false;
-    syncPulses = 0;
-    syncSeconds = 0;
+  processCommand();
+
+  if (digitalRead(PIN_FAST) == LOW && !fast) {
+    fast = true;
+    override(255,0);
+  }
+  if (digitalRead(PIN_FAST) == HIGH && fast) {
+    fast = false;
+    track();
   }
 
-  #ifndef DEBUG
-  LowPower.idle(SLEEP_120MS, ADC_OFF, TIMER2_ON, TIMER1_ON, TIMER0_ON, SPI_OFF, USART0_OFF, TWI_OFF);
-  #endif
+  // check override time
+  if (opMode == Override) {
+    if (overrideMilliseconds > 0 && millis() - overrideStartMillis > overrideMilliseconds) {
+      track();
+    }
+  }
+
+  switch (opMode) {
+    case Tracking:
+      duty = trackingDuty;
+      break;
+    case Override:
+      duty = overrideDuty;
+      break;
+  }
+
+  analogWrite(PIN_PWM, duty);
 }
